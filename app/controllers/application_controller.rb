0ea51a778bd70b86b0672734ee73970105aac3a7
Titus <titus.kamunya@student.moringaschool.com>
2022-09-10 11:25:59 +0300

    defined get routes on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,43 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/courses' do
+    all_courses = Course.all
+    all_courses.to_json(only: [:id, :course_name, :course_period, :total_units, :fees_amount])
+  end
+
+  get '/students' do
+    all_students = Student.all
+    all_students.to_json(only: [:id, :name, :race, :planet, :course_id, :teacher_id])
+  end
+
+  get '/teachers' do
+    all_teachers = Teacher.all
+    all_teachers.to_json(only: [:id, :name, :specialisation, :street_address, :salary])
+  end
+
+  get '/students/:id' do
+    single_student = Student.find(params[:id])
+    single_student.to_json(only: [:id, :name, :race, :planet], include: {
+      course: { only: [:course_name]}
+    })
+  end
+
+  get '/teachers/:id' do
+    single_teacher = Teacher.find(params[:id])
+    single_teacher.to_json(only: [:id, :name, :specialisation, :street_address, :salary],
+      include: {students: {only: [:name], include: {course: {only: [:course_name]}}}}
+    )
+  end
+
+  get '/courses/:id' do
+    single_course = Course.find(params[:id])
+    single_course.to_json(only: [:id, :course_name, :course_period, :total_units, :fees_amount], include: {students: {only: [:name]}})
+  end
+
+  get '/courses/edit/:id' do
+    single_course = Course.find(params[:id])
+    single_course.to_json(only: [:id, :course_period, :total_units, :fees_amount], include: {students: {only: [:name]}})
+  end
+
 end
